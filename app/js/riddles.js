40d7ac81ac73a452918b6fb801918b656efc853b
Paul Carduner <paul@carduner.net>
2014-10-14 13:11:44 -0700

    Add support for user account creation and auth
    I don't think users should be forced to create an account from the beginning, so this should change, but good enough for now.

--- app/js/riddles.js
+++ app/js/riddles.js
@@ -104,7 +104,8 @@ var RiddlesScreen = React.createClass({
     return {
       showRiddle: "r-1",
       completedRiddles: {},
-      livesLeft: 3
+      livesLeft: 3,
+      hintsLeft: 4,
     }
   },
 
@@ -153,6 +154,9 @@ var RiddlesScreen = React.createClass({
             <div className="riddleStat">
               Lives: <strong>{this.state.livesLeft}</strong>
             </div>
+            <div className="riddleStat">
+              Hints: <strong>{this.state.hintsLeft}</strong>
+            </div>
           </div>
           <div className="riddleLines">
             {lines}
@@ -169,12 +173,115 @@ var RiddlesScreen = React.createClass({
   }
 })
 
+var CreateAccountScreen = React.createClass({
+  getInitialState: function() {
+    return {
+      showLoginScreen: false,
+      isCreatingAccount: false,
+      isLoggingIn: false
+    };
+  },
+
+  handleCreateAccountClick: function() {
+    var username = this.refs.usernameField.getDOMNode().value;
+    var password = this.refs.passwordField.getDOMNode().value;
+    if (password != this.refs.passwordConfirmField.getDOMNode().value) {
+      alert("Passwords do not match.");
+    } else {
+      this.setState({isCreatingAccount:true});
+      var user = new Parse.User();
+      user.set('username', username);
+      user.set('password', password);
+      user.signUp(null, {
+        success: function(user) {
+          RiddlesApp.instance.setState({user:user});
+        },
+        error: function(user, error) {
+          alert("Something went wrong: "+error.code+' '+error.message);
+        }
+      })
+    }
+  },
+
+  handleShowLoginClick: function(shouldShowLoginScreen) {
+    this.setState({showLoginScreen:shouldShowLoginScreen});
+  },
+
+  handleLoginClick: function() {
+    this.setState({isLoggingIn:true});
+    Parse.User.logIn(
+      this.refs.usernameField.getDOMNode().value,
+      this.refs.passwordField.getDOMNode().value,
+      {
+        success: function(user) {
+          RiddlesApp.instance.setState({user:user});
+        },
+        error: function(user, error) {
+          alert("Something went wrong: "+error.code+' '+error.message);
+        }
+      });
+  },
+
+  render: function() {
+
+    var content = null;
+    if (this.state.isCreatingAccount) {
+      // we are in the middle of creating an account, so say so.
+      content = <h3>Creating Account...</h3>;
+    } else if (this.state.showLoginScreen) {
+      // user clicked the login button, so show them that.
+      content = (
+        <div>
+          <h3>Log In</h3>
+          <div className="form-group">
+            <label>Username</label>
+            <input ref="usernameField" type="text" className="form-control"/>
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input ref="passwordField" type="password" className="form-control"/>
+          </div>
+          <button className="pull-left btn btn-default" onClick={this.handleShowLoginClick.bind(this, false)}>Sign Up</button>
+          <button className="pull-right btn btn-primary" onClick={this.handleLoginClick}>Login</button>
+        </div>
+        );
+    } else {
+      // Ask the user to create an account
+      content = (
+        <div>
+          <h3>Sign Up</h3>
+          <div className="form-group">
+            <label>Username</label>
+            <input ref="usernameField" type="text" className="form-control" placeholder="Something fun..." />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input ref="passwordField" type="password" className="form-control" placeholder="Something good..." />
+          </div>
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input ref="passwordConfirmField" type="password" className="form-control" placeholder="One more time..." />
+          </div>
+          <button className="pull-left btn btn-default" onClick={this.handleShowLoginClick.bind(this, true)}>Login</button>
+          <button className="pull-right btn btn-primary" onClick={this.handleCreateAccountClick}>Create Account</button>
+        </div>
+        );
+    }
+
+    return <div className="introScreen">
+      <h1>{"Bellow's Riddle Adventure"}</h1>
+      {content}
+    </div>
+  }
+});
+
 var RiddlesApp = React.createClass({
   getInitialState: function() {
     return {
       startupPercent: 0,
       riddles: {},
-      gameOver: false
+      gameOver: false,
+      user: null
     }
   },
 
@@ -196,6 +303,8 @@ var RiddlesApp = React.createClass({
     // simulate fetching of data and other app initialization
     this.simulateStartup();
 
+    this.setState({user: Parse.User.current()});
+
     $.ajax({
       url: '../json-db/riddles/riddles.json',
       dataType: 'json',
@@ -209,7 +318,9 @@ var RiddlesApp = React.createClass({
   },
 
   render: function() {
-    if (this.state.startupPercent < 100) {
+    if (!this.state.user) {
+      return <CreateAccountScreen />
+    } else if (this.state.startupPercent < 100) {
       return <StartupScreen percentComplete={this.state.startupPercent}/>;
     } else if (this.state.gameOver) {
       return <Screen><h1>Game Over</h1></Screen>;
